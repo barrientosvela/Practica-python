cd4a557ad22d70c32b45ab949f6f835214648a23
José Barrientos Vela <jbarrientosvela@gmail.com>
2023-01-30 14:04:41 +0100

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,19 @@
 print('Hola')
+nom = "jose"
+ape = "Barrientos"
+num = 3
+print(type(nom))
+print(isinstance(num, int))  # Devuelve true o false
+
+print(nom, ape, sep=', ')
+nom = input('nombre:')  # cambia el valor de la variable
+print(nom, ape, sep=', ')
+"""
+Esto es un comentario
+de varias lineas
+"""
+# Un input siempre se guarda como un string
+edad = input()
+print(type(edad))
+edad = int(edad)  # cambia el tipo de dato
+print(type(edad))
